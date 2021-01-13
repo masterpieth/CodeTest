f0cbedf6a4622b68a30e9e6b6acfb1c75677a29c
masterpieth <kny0339@gmail.com>
2021-01-13 22:14:34 +0900

    코딩테스트 종합정리용 프로젝트

---
+++ CodeTest/src/BufferedIO.java
@@ -0,0 +1,13 @@
+
+public class BufferedIO {
+	public static void main(String[] args) {
+		/* 버퍼(Buffer)
+		 * : 데이터를 한 곳에서 다른 곳으로 전송하는 동안 일시적으로 그 데이터를 보관하는 임시 메모리 영역
+		 * 입출력 향상을 위해 사용함
+		 * 
+		 * 버퍼 플러시(Buffer flush)
+		 * : 버퍼에 남아있는 데이터를 출력시킴(버퍼를 비우는 동작)
+		 * 
+		 *  */
+	}
+}
