4b06e28b2f06735a8efd5b0fdd38dd2594b3e58b
nykim <kny0339@gmail.com>
2021-04-08 11:44:11 +0900

    2021-04-08 백준 1874번

---
+++ CodeTest/src/stack/b_1874.java
@@ -0,0 +1,36 @@
+package stack;
+import java.util.*;
+import java.io.*;
+
+public class b_1874 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		Stack<Integer> s = new Stack<>();
+		int start = 0;
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		while(n-- > 0) {
+			int value = Integer.parseInt(br.readLine());
+			
+			if(start < value) {
+				for(int i = start + 1; i <= value; i++) {
+					s.push(i);
+					sb.append("+\n");
+				}
+				start = value;
+			}
+			else if (s.peek() != value) {
+				System.out.println("NO");
+				return;
+			}
+			
+			s.pop();
+			sb.append("-\n");
+		}
+		System.out.println(sb.toString());
+	}
+}
