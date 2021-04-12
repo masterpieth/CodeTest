e53ed26239c3f89c6bf402ab06c9e1a1aade6e9b
nykim <kny0339@gmail.com>
2021-04-12 14:25:03 +0900

    2021-04-12 백준 1406번

---
+++ CodeTest/src/stack/b_1406.java
@@ -0,0 +1,49 @@
+package stack;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class b_1406 {
+
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Stack<Character> s1 = new Stack<>();
+        Stack<Character> s2 = new Stack<>();
+        
+        String n = br.readLine();
+        for(int i = 0; i < n.length(); i++) {
+            s1.push(n.charAt(i));
+        }
+        int m = Integer.parseInt(br.readLine());
+        
+        while(m-- > 0) {
+            String cmd = br.readLine();
+            
+            if(cmd.equals("L")) {
+                if(!s1.isEmpty()) s2.push(s1.pop());
+            }
+            else if(cmd.equals("D")) {
+                if(!s2.isEmpty()) s1.push(s2.pop());
+            }
+            else if(cmd.equals("B")) {
+                if(!s1.isEmpty()) s1.pop();
+            }
+            else {
+                s1.push(cmd.charAt(2));
+            }
+        }
+        
+        while(!s1.isEmpty()) {
+            s2.push(s1.pop());
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        
+        while(!s2.isEmpty()) {
+            sb.append(s2.pop());
+        }
+        
+        System.out.println(sb.toString());
+    }
+}
