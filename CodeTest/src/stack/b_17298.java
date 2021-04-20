51ad3ddedf552a97bb7ba9acc8a833fab689d3bd
nykim <kny0339@gmail.com>
2021-04-20 09:59:54 +0900

    2021-04-20 백준 17298 Stack O(N)

--- CodeTest/src/stack/b_17298.java
+++ CodeTest/src/stack/b_17298.java
@@ -1,7 +1,6 @@
 package stack;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.Stack;
+import java.util.*;
 import java.io.*;
 
 
@@ -10,35 +9,31 @@ public class b_17298 {
 	public static void main(String[] args) throws IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		Stack<Integer> s = new Stack<>();
 		
 		int n = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		
-		String str = br.readLine();
-        String[] sArr = str.split(" ");
-        
-        int[] arr = new int[n];
-        
-        Queue<Integer> q = new LinkedList<>();
-        
-        for(int i = 0; i < n; i++) {
-        	arr[i] = Integer.parseInt(sArr[i]);
-        }
-        
-        for(int i = 0; i < n-1; i++) {
-        	for(int j = i+1; j < n; j++) {
-        		if(arr[i] < arr[j]) {
-        			q.offer(arr[j]);
-        			break;
-        		}
-        	}
-        	if(q.size() != i+1) q.offer(-1);
-        }
-        q.offer(-1);
-        
-        while(!q.isEmpty()) {
-        	bw.write(q.poll() + " ");
-        }
-        bw.flush();
+		int[] arr = new int[n];
+		
+		for(int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		for(int i = 0; i < n; i++) {
+			while(!s.isEmpty() && arr[s.peek()] < arr[i]) {
+				arr[s.pop()] = arr[i];
+			}
+			s.push(i);
+		}
+		
+		while(!s.isEmpty()) {
+			arr[s.pop()] = -1;
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		for(int i : arr) sb.append(i + " ");
+		
+		System.out.println(sb);
 	}
 }
