1a716754eeddaedbacf2ceba2e1675c47a3626b7
NYK <kny0339@gmail.com>
2022-09-05 15:44:36 +0900

    2022-09-05 two pointers 1.두 배열 합치기 셀프코드

--- CodeTest/src/tpsw/J01.java
+++ CodeTest/src/tpsw/J01.java
@@ -2,29 +2,45 @@ package tpsw;
 import java.util.*;
 
 public class J01 {
-	public ArrayList<Integer> solution(ArrayList<Integer> arr1, ArrayList<Integer> arr2) {
-		for(int i=0; i<arr1.size(); i++) {
-			arr2.add(arr1.get(i));
+	public ArrayList<Integer> solution(int n, int[] a, int m, int[] b) {
+		ArrayList<Integer> answer = new ArrayList<>();
+		int p1=0, p2=0;
+		while(p1<n && p2<m) {
+			if(a[p1] < b[p2]) {
+				answer.add(a[p1]);
+				p1++;
+			}
+			else {
+				answer.add(b[p2]);
+				p2++;
+			}
 		}
-		arr2.sort(Comparator.naturalOrder());
-		
-		return arr2;
+		if(p1<n) {
+			for(int i=p1; i<n; i++) {
+				answer.add(a[i]);
+			}
+		}
+		if(p2<m) {
+			for(int i=p2; i<m; i++) {
+				answer.add(b[i]);
+			}
+		}
+		return answer;
 	}
-	
 	public static void main(String[] args) {
 		J01 J = new J01();
 		Scanner sc = new Scanner(System.in);
-		int n1 = sc.nextInt();
-		ArrayList<Integer> arr1 = new ArrayList<>();
-		for(int i=0; i<n1; i++) {
-			arr1.add(sc.nextInt());
+		int n = sc.nextInt();
+		int[] a = new int[n];
+		for(int i=0; i<n; i++) {
+			a[i] = sc.nextInt();
 		}
-		int n2 = sc.nextInt();
-		ArrayList<Integer> arr2 = new ArrayList<>();
-		for(int i=0; i<n2; i++) {
-			arr2.add(sc.nextInt());
+		int m = sc.nextInt();
+		int[] b = new int[m];
+		for(int i=0; i<m; i++) {
+			b[i] = sc.nextInt();
 		}
-		for(int x : J.solution(arr1,arr2)) {
+		for(int x: J.solution(n,a,m,b)) {
 			System.out.print(x + " ");
 		}
 	}
