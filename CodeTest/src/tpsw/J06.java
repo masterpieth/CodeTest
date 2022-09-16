e91591eb0370157c5d6e86072455fb91d2f93ace
NYK <kny0339@gmail.com>
2022-09-16 15:48:50 +0900

    2022-09-16 two pointers 6.최대 길이 연속부분수열

---
+++ CodeTest/src/tpsw/J06.java
@@ -0,0 +1,28 @@
+package tpsw;
+import java.util.*;
+
+public class J06 {
+	public int solution(int n, int k, int[] arr) {
+		int answer = 0, cnt = 0, lt = 0;
+		for(int rt=0; rt<n; rt++) {
+			if(arr[rt]==0) cnt++;
+			while(cnt>k) {
+				if(arr[lt]==0) cnt--;
+				lt++;
+			}
+			answer = Math.max(answer, rt-lt+1);
+		}
+		return answer;
+	}
+	public static void main(String[] args) {
+		J06 J = new J06();
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int k = sc.nextInt();
+		int[] arr = new int[n];
+		for(int i=0; i<n; i++) {
+			arr[i] = sc.nextInt();
+		}
+		System.out.println(J.solution(n,k,arr));
+	}
+}
