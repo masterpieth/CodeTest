ecf08c28e98fe868967c49a52df739f8f005d940
NYK <kny0339@gmail.com>
2022-09-20 08:41:20 +0900

    2022-09-20 hashmap 2.아나그램 정답코드

--- CodeTest/src/hash/J02.java
+++ CodeTest/src/hash/J02.java
@@ -4,16 +4,16 @@ import java.util.*;
 public class J02 {
 	public String solution(String s1, String s2) {
 		String answer = "YES";
-		HashMap<Character, Integer> map1 = new HashMap<>();
-		HashMap<Character, Integer> map2 = new HashMap<>();
+		HashMap<Character, Integer> map = new HashMap<>();
+		//s1을 map에 담는다
 		for(char x : s1.toCharArray()) {
-			map1.put(x, map1.getOrDefault(x, 0)+1);
+			map.put(x, map.getOrDefault(x, 0)+1);
 		}
 		for(char x : s2.toCharArray()) {
-			map2.put(x, map2.getOrDefault(x, 0)+1);
-		}
-		for(char key : map1.keySet()) {
-			if(map1.get(key) != map2.get(key)) answer = "NO";
+			//x라는 키값이 없거나, 줄이려고 봤더니 value가 0인경우 NO를 반환한다.
+			if(!map.containsKey(x) || map.get(x)==0) return "NO";
+			//key 값이 있는 경우에는 value를 1 줄인다.
+			map.put(x, map.get(x)-1);
 		}
 		return answer;
 	}
