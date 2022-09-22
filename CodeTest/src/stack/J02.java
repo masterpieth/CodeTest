425f8ade1f61bc4da647e5ac814d0a88e32a7efd
NYK <kny0339@gmail.com>
2022-09-22 14:41:41 +0900

    2022-09-22 stack 2.괄호문자제거 셀프코드 수정

--- CodeTest/src/stack/J02.java
+++ CodeTest/src/stack/J02.java
@@ -1,13 +1,24 @@
 package stack;
 import java.util.*;
+import java.util.Stack;
 
 public class J02 {
 	public String solution(String str) {
 		StringBuilder answer = new StringBuilder();
+		Stack<Character> s = new Stack<>();
 		for(char x : str.toCharArray()) {
-			if(Character.isAlphabetic(x)) answer.append(x);
+			if(x !=')') s.push(x);
+			else {
+				while(true) {
+					char flag = s.pop();
+					if(flag=='(') break;
+				}
+			}
 		}
-		return answer.toString();
+		while(!s.isEmpty()) {
+			answer.append(s.pop());
+		}
+		return answer.reverse().toString();
 	}
 	
 	public static void main(String[] args) {
