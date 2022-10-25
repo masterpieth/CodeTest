6948fe480c378e3e50d34f5468c402dd5fea870a
NYK <kny0339@gmail.com>
2022-10-25 19:56:02 +0900

    2022-10-25 버블정렬 셀프코드

---
+++ CodeTest/src/ss/J02.java
@@ -0,0 +1,28 @@
+package ss;
+import java.util.*;
+
+public class J02 {
+	public int[] solution(int[] arr) {
+		for(int i=0; i<arr.length; i++) {
+			for(int j=0; j<arr.length-1;j++) {
+				int temp = 0;
+				if(arr[j]>arr[j+1]) {
+					temp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = temp;
+				}
+			}
+		}
+		return arr;
+	}
+	public static void main(String[] args) {
+		J02 J = new J02();
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		for(int i=0; i<n; i++) {
+			arr[i] = sc.nextInt();
+		}
+		for(int x : J.solution(arr)) System.out.print(x + " ");
+	}
+}
